2b0d0a47da285460272008c24cf4d56ffe6bf07d
fly-sonic <>
2024-10-31 20:48:50 -0400

    Update server: listen to simple socket events, connection & disconnect

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // TODO
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected...`);
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected!`);
+  });
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
